2748ab055e3184d273373021ff91370d0b851c9c
Kavya Gundeti <kavyagundeti9@gmail.com>
2026-05-24 15:52:26 +0530

    Delete frontend/Quiz-Frontend-Angular-master/Quiz-Frontend-Angular-master directory

--- frontend/Quiz-Frontend-Angular-master/Quiz-Frontend-Angular-master/src/app/exit-serve.guard.ts
+++
@@ -1,6 +0,0 @@
-import { CanDeactivateFn } from '@angular/router';
-import { ResultComponent } from './result/result.component';
-
-export const exitServeGuard: CanDeactivateFn<ResultComponent> = (component, currentRoute, currentState, nextState) => {
-  return component.canExit();
-};
