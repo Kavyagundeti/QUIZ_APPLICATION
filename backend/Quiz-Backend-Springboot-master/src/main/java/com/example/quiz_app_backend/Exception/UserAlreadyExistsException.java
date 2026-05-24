b294b12372ba812dd114a1aa0a820fca2aa42319
Kavya Gundeti <kavyagundeti9@gmail.com>
2026-05-24 15:39:49 +0530

    Delete backend/Quiz-Backend-Springboot-master directory

--- backend/Quiz-Backend-Springboot-master/src/main/java/com/example/quiz_app_backend/Exception/UserAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.example.quiz_app_backend.Exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message)
-    {
-        super(message);
-    }
-}
