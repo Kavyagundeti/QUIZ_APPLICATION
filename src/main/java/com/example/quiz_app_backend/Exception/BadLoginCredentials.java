7c7ba3fcf79403484d6e3688dadfd837d7f04fb5
Kavya Gundeti <kavyagundeti9@gmail.com>
2026-05-24 15:45:18 +0530

    Delete src directory

--- src/main/java/com/example/quiz_app_backend/Exception/BadLoginCredentials.java
+++
@@ -1,7 +0,0 @@
-package com.example.quiz_app_backend.Exception;
-
-public class BadLoginCredentials extends RuntimeException{
-    public BadLoginCredentials(String message){
-        super(message);
-    }
-}
