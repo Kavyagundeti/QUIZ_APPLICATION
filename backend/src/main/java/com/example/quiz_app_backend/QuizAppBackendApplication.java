e441dfb76235159f51df2ca82ab52fb8c69a66ef
Kavya Gundeti <kavyagundeti9@gmail.com>
2026-05-25 18:30:55 +0530

    Update QuizAppBackendApplication.java

--- backend/src/main/java/com/example/quiz_app_backend/QuizAppBackendApplication.java
+++ backend/src/main/java/com/example/quiz_app_backend/QuizAppBackendApplication.java
@@ -2,15 +2,18 @@ package com.example.quiz_app_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class QuizAppBackendApplication {
+public class QuizAppBackendApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuizAppBackendApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(QuizAppBackendApplication.class);
+    }
 
-
-
+    public static void main(String[] args) {
+        SpringApplication.run(QuizAppBackendApplication.class, args);
+    }
 }
-
